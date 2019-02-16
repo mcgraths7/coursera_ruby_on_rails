f70fe32600d92fa780b4afb59edec35bc9d61af1
Steven McGrath <mcgrath.steven7@gmail.com>
2019-02-16 12:39:36 -0500

    added session controller and a ensured user is logged in before they can view pages

--- action_pack/i_reviewed/config/routes.rb
+++ action_pack/i_reviewed/config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  root to: "books#index"
+
+  get "/login" => "sessions#new", as: "login"
+  delete "/logout" => "sessions#destroy", as: "logout"
+  
   resources :books do
     resources :notes, only: [:create, :destroy]
   end
+  resources :sessions, only: [:create, :new, :destroy]
 
-  root to: "books#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

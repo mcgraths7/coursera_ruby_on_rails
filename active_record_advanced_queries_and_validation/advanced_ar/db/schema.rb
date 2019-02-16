2d002b7dd410e6ca0a50074b2ddb8fbf0c4e42b2
Steven McGrath <mcgrath.steven7@gmail.com>
2019-02-16 12:41:11 -0500

    added unadded files

--- active_record_advanced_queries_and_validation/advanced_ar/db/schema.rb
+++ active_record_advanced_queries_and_validation/advanced_ar/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190210001647) do
+ActiveRecord::Schema.define(version: 20190212151523) do
 
   create_table "hobbies", force: :cascade do |t|
     t.string   "name"
@@ -42,8 +42,10 @@ ActiveRecord::Schema.define(version: 20190210001647) do
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.string   "hometown"
+    t.integer  "number_of_dogs"
   end
 
   create_table "personal_infos", force: :cascade do |t|
